975614750f9f0efe1c2b8f5737971775bf35e579
yannick sielski <yannick.sielski@nara.to>
2023-01-12 16:36:00 +0100

    Changed the app component css file to scss

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -12,7 +12,7 @@ import { Store } from '@ngrx/store';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit, IHaveLoader {
 
